0332d573f521bec0e606ecd39972d90fd65593d6
Eelco M. Glasl <eelco.glasl@gmail.com>
2017-01-01 17:21:23 +0100

    Added new method for getting LIP

--- ookla.sh
+++ ookla.sh
@@ -13,7 +13,7 @@
 # 2. Change DNF if necessary for different file size
 # 3. Create a cronjob with content as shown below
 # 4. Check if LOG gets created
-# 5. LOG can be opened with spreadsheet to create bandwidth graphs during 1 month
+# 5. LOG can be opened with spreadsheet to create graphs during 1 month
 # 6. Create symbolic links from ./ookla.sh to ./incoming and ./outgoing
 #
 # CRONJOB:
@@ -32,21 +32,26 @@ BIN="/usr/bin/curl"                       # cURL binary
 URL="ftp://speedtest.tele2.net/"          # Download URL of OOKLA
 API="https://api.ipify.org?format=text"   # Get my public IP in text format
 DNF="10MB.zip"                            # Download file to test bandwidth
-UPF="upload/$(/bin/date +%N).zip"         # Upload file name, unique
-LOG="${WWW}$(/bin/date +%Y-%m)"           # Create a new log every month
+UPF="upload/$(date +%N).zip"              # Upload file name, unique
+LOG="${WWW}$(date +%Y-%m)"                # Create a new log every month
+
 # Format of curl date (for curl -w)
 FMT="%{remote_ip},%{size_download},%{speed_download},%{size_upload},%{speed_upload},%{time_namelookup},%{time_connect},%{time_total}"
-# Get my local public IP address via API but check before use
-PIP="$(${BIN} -f -s ${API} | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')"
-# Get my local IP address or hostname
-LIP="$(host $(hostname -s) | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')"
+
+# Get my public IP address via API but check output before use
+PIP="$(${BIN} -f -s ${API} | egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')"
+
+# Get my local IP address of external NIC
+# LIP="$(host $(hostname -s) | egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')"
+LIP="$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')"
+
+# Get my stripped basename
+SELF="${0}"; BASE="${SELF##*/}"; CORE="${BASE%.*}"
+
 
 #######################
 ##  Basic functions  ##
 #######################
-
-# Get stripped basename of called script
-SELF="${0}"; BASE="${SELF##*/}"; CORE="${BASE%.*}"
 
 function testdata {
   # If test data file for upload does not exist, download it first
@@ -55,26 +60,30 @@ function testdata {
 
 function logfiles {
   # Create log file for the first time
-  echo -e "time_stamp,data_flow,local_ip,public_ip,${FMT}" | sed -e 's/[%{}]//g' >${OUT}
+  echo -e "time_stamp,data_flow,local_ip,public_ip,${FMT}" \
+    | sed -e 's/[%{}]//g' >${OUT}
 }
 
 function incoming {
   # echo "Script \"${CORE}\" on public IP ${PIP} testing download speed!"
   # Set time stamp
-  TST="$(/bin/date +%Y-%m-%dT%H:%M:%S%z)"
+  TST="$(date +%Y-%m-%dT%H:%M:%S%z)"
   # Perform the download
-  echo "${TST},incoming,${LIP},${PIP},${FMT}\n" | ${BIN} -w "@-" -s ${URL}${DNF} \
+  echo "${TST},incoming,${LIP},${PIP},${FMT}\n" \
+    | ${BIN} -w "@-" -s ${URL}${DNF} \
     -o /dev/null >>${OUT}
 }
 
 function outgoing {
   # echo "Script \"${CORE}\" on public IP ${PIP} testing upload speed!"
   # Set time stamp
-  TST="$(/bin/date +%Y-%m-%dT%H:%M:%S%z)"
+  TST="$(date +%Y-%m-%dT%H:%M:%S%z)"
   # Perform the upload
-  echo "${TST},outgoing,${LIP},${PIP},${FMT}\n" | ${BIN} -w "@-" -T ${DIR}${DNF} \
+  echo "${TST},outgoing,${LIP},${PIP},${FMT}\n" \
+    | ${BIN} -w "@-" -T ${DIR}${DNF} \
     -s ${URL}${UPF} >>${OUT}
 }
+
 
 ###################
 ##  Main script  ##
@@ -109,6 +118,10 @@ case ${CORE} in
 
   * )
     echo "Usage: [ combined | incoming | outgoing ]"
+    echo "OOKLA Speedtest wrapper, (c) 2017 by eelco.glasl@gmail.com"
+    echo "  combined - write combined download and upload log"
+    echo "  incoming - write download log"
+    echo "  outgoing - write upload log"
     exit 1
     ;;
 
